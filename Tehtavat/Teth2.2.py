0a4a81836731e18b3b2b4bfd0307e6159d90df54
EmmanV <velosoemmanuel336@gmail.com>
2025-08-28 17:44:48 +0300

    Tehtävä 2.2

---
+++ Tehtavat/Teth2.2.py
@@ -0,0 +1,16 @@
+# A=pi*r**2 (ympyrä pinta-ala kaava)
+import math
+
+radius=int(input("Enter the radius of the circle: "))
+area= (math.pi*radius**2)
+
+print(f"The area of the circle is: , {area:.2f}")
+
+
+
+
+
+
+
+
+
